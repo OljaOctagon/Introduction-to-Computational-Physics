b89fc473d03c2e3ac208efec8f1301b1dfe6d514
Cat Nate <catnat@protonmail.com>
2023-08-22 09:13:40 +0200

    generate starting state works now

--- lammps/moving-patches/generate_starting_state/generate_starting_state.py
+++ lammps/moving-patches/generate_starting_state/generate_starting_state.py
@@ -5,6 +5,8 @@ npart = 225
 npatch = 2
 natoms = npart * (npatch + 1)
 nbonds = npart * npatch
+# diameter of core
+sigma = 1
 
 lx2 = 10
 ly2 = lx2
@@ -29,14 +31,19 @@ masses = [{"type": 1, "mass": 2.0}, {"type": 2, "mass": 0.5}]
 context["masses"] = masses
 
 atoms = []
-for i in range(1, npart + 1):
+bonds = []
+angles = []
+for i in range(npart):
     xcore, ycore = np.random.rand(2) * 2 * lx2 - lx2
-    zcore = np.random.rand() * 2 * lz2 - lz2
+    zcore = 0
+
+    core_id = i * 3 + 1
 
     core_atom = {
-        "atom_id": i * 3,
-        "mol_id": i,
+        "atom_id": core_id,
+        "mol_id": i + 1,
         "atom_type": 1,
+        "charge": 1,
         "x": xcore,
         "y": ycore,
         "z": zcore,
@@ -45,22 +52,41 @@ for i in range(1, npart + 1):
     atoms.append(core_atom)
 
     for j in range(1, npatch + 1):
-        a = 1  # needs to be changed to right distance
-        b = 1  # needs to be changed to right distance
-
+        patch_id = core_id + j
         patch_atom = {
-            "atom_id": i * 3 + j,
-            "mol_id": i,
+            "atom_id": patch_id,
+            "mol_id": i + 1,
             "atom_type": 2,
-            "x": xcore + a,
-            "y": ycore + b,
+            "charge": 1,
+            "x": xcore + np.power(-1, j) * sigma / 2,
+            "y": ycore,
             "z": zcore,
         }
-
         atoms.append(patch_atom)
+
+        bond = {
+            "id": i * 2 + j,
+            "bond_type": 1,
+            "atom_1": core_id,
+            "atom_2": patch_id,
+        }
+        bonds.append(bond)
+
+    angle = {
+        "id": i + 1,
+        "angle_type": 1,
+        "atom_1": core_id + 1,
+        "atom_2": core_id,
+        "atom_3": core_id + 2,
+    }
+
+    angles.append(angle)
 
 
 context["atoms"] = atoms
+context["bonds"] = bonds
+context["angles"] = angles
+
 environment = Environment(loader=FileSystemLoader("templates/"))
 template = environment.get_template("template_start.txt")
 
