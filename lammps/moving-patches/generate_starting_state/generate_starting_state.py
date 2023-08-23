bb9f7597ef94832274fe441bf083dfc8684cdfc9
Cat Nate <catnat@protonmail.com>
2023-08-23 16:37:41 +0200

    added restart script for pentagon particles in moving_patches

--- lammps/moving-patches/generate_starting_state/generate_starting_state.py
+++ lammps/moving-patches/generate_starting_state/generate_starting_state.py
@@ -2,9 +2,10 @@ from jinja2 import Environment, FileSystemLoader
 import numpy as np
 
 npart = 225
-npatch = 2
+npatch = 5
 natoms = npart * (npatch + 1)
 nbonds = npart * npatch
+nangles = npart * npatch
 # diameter of core
 sigma = 1
 
@@ -15,7 +16,7 @@ lz2 = 0.5
 context = {
     "number_of_atoms": natoms,
     "number_of_bonds": nbonds,
-    "number_of_angles": npart,
+    "number_of_angles": nangles,
     "number_of_atom_types": 2,
     "number_of_bond_types": 1,
     "number_of_angle_types": 1,
@@ -30,6 +31,28 @@ context = {
 masses = [{"type": 1, "mass": 2.0}, {"type": 2, "mass": 0.5}]
 context["masses"] = masses
 
+ru = sigma / 2
+alpha = (108 / 180) * np.pi
+mu = (72 / 180) * np.pi
+
+ax = ru * np.sin(mu / 2)
+ay = ru * np.cos(mu / 2)
+
+bx = ru * np.sin(alpha / 2)
+by = ru * np.cos(alpha / 2)
+
+patch_center_distance = {
+    1: (-ax, -ay),
+    2: (ax, -ay),
+    3: (bx, by),
+    4: (0, ru),
+    5: (-bx, by),
+}
+
+arr = np.array([[-ax, -ay], [ax, -ay], [bx, by], [0, ru], [-bx, by]])
+for v in arr:
+    assert np.linalg.norm(v) == ru
+
 atoms = []
 bonds = []
 angles = []
@@ -37,7 +60,7 @@ for i in range(npart):
     xcore, ycore = np.random.rand(2) * 2 * lx2 - lx2
     zcore = 0
 
-    core_id = i * 3 + 1
+    core_id = i * (npatch + 1) + 1
 
     core_atom = {
         "atom_id": core_id,
@@ -58,29 +81,29 @@ for i in range(npart):
             "mol_id": i + 1,
             "atom_type": 2,
             "charge": 1,
-            "x": xcore + np.power(-1, j) * sigma / 2,
-            "y": ycore,
+            "x": xcore + patch_center_distance[j][0],
+            "y": ycore + patch_center_distance[j][1],
             "z": zcore,
         }
         atoms.append(patch_atom)
 
         bond = {
-            "id": i * 2 + j,
+            "id": i * npatch + j,
             "bond_type": 1,
             "atom_1": core_id,
             "atom_2": patch_id,
         }
         bonds.append(bond)
 
-    angle = {
-        "id": i + 1,
-        "angle_type": 1,
-        "atom_1": core_id + 1,
-        "atom_2": core_id,
-        "atom_3": core_id + 2,
-    }
+        angle = {
+            "id": i + 1,
+            "angle_type": 1,
+            "atom_1": core_id + j,
+            "atom_2": core_id,
+            "atom_3": core_id + (j % npatch) + 1,
+        }
 
-    angles.append(angle)
+        angles.append(angle)
 
 
 context["atoms"] = atoms
