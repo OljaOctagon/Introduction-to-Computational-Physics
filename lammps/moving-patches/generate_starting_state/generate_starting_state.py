3a44325a228fec3e8fbfe3bc57d61cbdc088e843
Cat Nate <catnat@protonmail.com>
2023-08-29 10:17:09 +0200

    starting state generator changed to four patch systems

--- lammps/moving-patches/generate_starting_state/generate_starting_state.py
+++ lammps/moving-patches/generate_starting_state/generate_starting_state.py
@@ -1,17 +1,19 @@
 from jinja2 import Environment, FileSystemLoader
 import numpy as np
 
-npart = 500
-npatch = 5
+npart = 300
+npatch = 4
 natoms = npart * (npatch + 1)
 nbonds = npart * npatch
-nangles = npart * npatch
+# nangles = npart * npatch
+nangles = npart * (npatch + 2)
+
 # diameter of core
 sigma = 1
 
-lx2 = 15
+lx2 = 7
 ly2 = lx2
-lz2 = 0.5
+lz2 = lx2
 
 rho = npart / (2 * lx2 * 2 * ly2 * 2 * lz2)
 print("number density central particles: ", rho)
@@ -26,7 +28,7 @@ context = {
     "number_of_angles": nangles,
     "number_of_atom_types": 2,
     "number_of_bond_types": 1,
-    "number_of_angle_types": 1,
+    "number_of_angle_types": 2,
     "xlo": -lx2,
     "xhi": lx2,
     "ylo": -lx2,
@@ -38,34 +40,50 @@ context = {
 masses = [{"type": 1, "mass": 2.0}, {"type": 2, "mass": 0.5}]
 context["masses"] = masses
 
-ru = sigma / 2
-alpha = (108 / 180) * np.pi
-mu = (72 / 180) * np.pi
 
-ax = ru * np.sin(mu / 2)
-ay = ru * np.cos(mu / 2)
+def get_pentagon_distances(sigma):
+    ru = sigma / 2
+    alpha = (108 / 180) * np.pi
+    mu = (72 / 180) * np.pi
 
-bx = ru * np.sin(alpha / 2)
-by = ru * np.cos(alpha / 2)
+    ax = ru * np.sin(mu / 2)
+    ay = ru * np.cos(mu / 2)
 
-patch_center_distance = {
-    1: (-ax, -ay),
-    2: (ax, -ay),
-    3: (bx, by),
-    4: (0, ru),
-    5: (-bx, by),
-}
+    bx = ru * np.sin(alpha / 2)
+    by = ru * np.cos(alpha / 2)
 
-arr = np.array([[-ax, -ay], [ax, -ay], [bx, by], [0, ru], [-bx, by]])
-for v in arr:
-    assert np.linalg.norm(v) == ru
+    dist = {
+        1: (-ax, -ay),
+        2: (ax, -ay),
+        3: (bx, by),
+        4: (0, ru),
+        5: (-bx, by),
+    }
+
+    return dist
+
+
+def get_square_distances(sigma):
+    r = sigma / 2
+    dist = {
+        1: (-r, 0),
+        2: (0, r),
+        3: (r, 0),
+        4: (0, -r),
+    }
+    return dist
+
+
+patch_center_distance = {}
+patch_center_distance[4] = get_square_distances(sigma)
+patch_center_distance[5] = get_pentagon_distances(sigma)
 
 atoms = []
 bonds = []
 angles = []
 for i in range(npart):
-    xcore, ycore = np.random.rand(2) * 2 * lx2 - lx2
-    zcore = 0
+    xcore, ycore, zcore = np.random.rand(3) * 2 * lx2 - lx2
+    # zcore = 0
 
     core_id = i * (npatch + 1) + 1
 
@@ -88,8 +106,8 @@ for i in range(npart):
             "mol_id": i + 1,
             "atom_type": 2,
             "charge": 1,
-            "x": xcore + patch_center_distance[j][0],
-            "y": ycore + patch_center_distance[j][1],
+            "x": xcore + patch_center_distance[npatch][j][0],
+            "y": ycore + patch_center_distance[npatch][j][1],
             "z": zcore,
         }
         atoms.append(patch_atom)
@@ -108,6 +126,17 @@ for i in range(npart):
             "atom_1": core_id + j,
             "atom_2": core_id,
             "atom_3": core_id + (j % npatch) + 1,
+        }
+
+        angles.append(angle)
+
+    for j in range(1, 3):
+        angle = {
+            "id": i + 1,
+            "angle_type": 2,
+            "atom_1": core_id + j,
+            "atom_2": core_id,
+            "atom_3": core_id + j + 2,
         }
 
         angles.append(angle)
